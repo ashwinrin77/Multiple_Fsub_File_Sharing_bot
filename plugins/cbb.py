5eb837d2548ca7e9ed81e1bbaf7eb86e1fbfbc40
ashwinrin77 <161035206+ashwinrin77@users.noreply.github.com>
2024-05-29 00:44:19 -1200

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='0'>Click here</a>\n○ Channel : https://t.me/Animetoon_xyz\n○ Support Group : https://t.me/Animetoon_xyz</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='0'>Click here</a>\n○ Channel : https://t.me/animetamil_xyz\n○ Support Group : https://t.me/animetamil_xyz</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
